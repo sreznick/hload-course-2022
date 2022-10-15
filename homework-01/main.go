606176b490d4b7dffc3a7a1d0211ef698cffcaa2
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2022-10-15 18:55:36 +0300

    new main

--- homework-01/main.go
+++ homework-01/main.go
@@ -1,51 +1,175 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "net/http"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net"
+	"strings"
+	"time"
 
-    "github.com/gin-gonic/gin"
-    _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/ssh"
 )
 
-const SQL_DRIVER = "postgres"
-const SQL_CONNECT_URL = "postgres://postgres:postgres@localhost"
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
+const (
+	CERT_PASSWORD        = 1
+	CERT_PUBLIC_KEY_FILE = 2
+	DEFAULT_TIMEOUT      = 3 // second
+)
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		if user == "vasya" {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": "12345"})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	return r
+type SSH struct {
+	Ip      string
+	User    string
+	Cert    string //password or key file path
+	Port    int
+	session *ssh.Session
+	client  *ssh.Client
 }
 
+type DBConnect struct {
+	Ip   string
+	User string
+	Cert string
+	Name string
+
+	db *sqlx.DB
+}
+
+type urlDB struct {
+	Key string `db:"short_url"`
+	URL string `db:"long_url"`
+}
+
+func randKey() string {
+	var build strings.Builder
+	build.Grow(10)
+	for i := 0; i < 10; i++ {
+		build.WriteRune(letters[rand.Intn(len(letters))])
+	}
+	return build.String()
+}
+
+func (sshClient *SSH) readPublicKeyFile(file string) ssh.AuthMethod {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		return nil
+	}
+	return ssh.PublicKeys(key)
+}
+
+func (sshClient *SSH) Connect(mode int) {
+
+	var ssh_config *ssh.ClientConfig
+	var auth []ssh.AuthMethod
+	if mode == CERT_PASSWORD {
+		auth = []ssh.AuthMethod{ssh.Password(sshClient.Cert)}
+	} else if mode == CERT_PUBLIC_KEY_FILE {
+		auth = []ssh.AuthMethod{sshClient.readPublicKeyFile(sshClient.Cert)}
+	} else {
+		log.Println("does not support mode: ", mode)
+		return
+	}
+
+	ssh_config = &ssh.ClientConfig{
+		User: sshClient.User,
+		Auth: auth,
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			return nil
+		},
+		Timeout: time.Second * DEFAULT_TIMEOUT,
+	}
+
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", sshClient.Ip, sshClient.Port), ssh_config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		fmt.Println(err)
+		client.Close()
+		return
+	}
+
+	sshClient.session = session
+	sshClient.client = client
+}
+
+func (sshClient *SSH) RunCmd(cmd string) {
+	out, err := sshClient.session.CombinedOutput(cmd)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(out))
+}
+
+func (sshClient *SSH) Close() {
+	sshClient.session.Close()
+	sshClient.client.Close()
+}
+
+func (dbClient *DBConnect) Open() {
+	db, err := sqlx.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", dbClient.User, dbClient.Cert, dbClient.Ip, dbClient.Name))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	dbClient.db = db
+}
+
+func (dbClient *DBConnect) Close() {
+	dbClient.db.Close()
+}
+
+func (dbClient *DBConnect) store(key string, url string) {
+	dbClient.db.MustExec("INSERT INTO urls (short_url, long_url) VALUES ($1, $2)", key, url)
+}
+
+func (dbClient *DBConnect) loadKey(url string) (key string, ok bool) {
+	el := urlDB{}
+	dbClient.db.Get(&el, "SELECT * FROM urls WHERE long_url=$1", url)
+	key, ok = el.Key, el.Key != ""
+	return
+}
+
+func (dbClient *DBConnect) loadURL(key string) (url string, ok bool) {
+	el := urlDB{}
+	dbClient.db.Get(&el, "SELECT * FROM urls WHERE short_url=$1", key)
+	url, ok = el.URL, el.URL != ""
+	return
+}
+
+// demo
 func main() {
-    fmt.Println(sql.Drivers())
-    conn, err := sql.Open(SQL_DRIVER, SQL_CONNECT_URL)
-    if err != nil {
-        fmt.Println("Failed to open", err)
-        panic("exit")
-    }
+	server := &SSH{
+		Ip:   "217.25.88.166",
+		User: "root",
+		Port: 22,
+		Cert: "Rhs:jgjkm166",
+	}
 
-    err = conn.Ping()
-    if err != nil {
-        fmt.Println("Failed to ping database", err)
-        panic("exit")
-    }
+	server.Connect(CERT_PASSWORD)
+	defer server.Close()
 
+	client := &DBConnect{
+		Ip:   "127.0.0.1",
+		User: "postgres",
+		Name: "url_shortener",
+		Cert: "Rhs:jgjkm166"}
 
-	r := setupRouter()
-	r.Run(":8080")
+	client.Open()
+	defer client.Close()
+
 }
