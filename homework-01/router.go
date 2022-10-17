adee45cbc1de223f56d726185d283045f036571d
Andrey Mozhaev <mozhay2000@gmail.com>
2022-10-17 16:49:07 +0300

    task 1

---
+++ homework-01/router.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	findByUrl = "SELECT id FROM urlsStorage WHERE long_url = $1"
+	findById  = "SELECT long_url FROM urlsStorage WHERE id = $1"
+	insertUrl = "INSERT INTO urlsStorage(long_url) VALUES ($1) ON CONFLICT DO NOTHING"
+)
+
+type PutRequestBody struct {
+	longurl string `json:"longurl"`
+}
+
+func putRequest(db *sql.DB, c *gin.Context) {
+	request := PutRequestBody{}
+	err := c.BindJSON(&request)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"response": "wrong longurl"})
+		return
+	}
+
+	longUrl := request.longurl
+	var id int
+
+	err = db.QueryRow(findByUrl, longUrl).Scan(&id)
+	if err != nil {
+		_, insert_err := db.Exec(insertUrl, longUrl)
+		if insert_err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"response": "insert error"})
+			return
+		}
+		_ = db.QueryRow(findByUrl, longUrl).Scan(&id)
+	}
+
+	tinyUrl, err := IdToTinyUrl(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"response": "wrong id"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"longurl": longUrl, "tinyurl": tinyUrl})
+}
+
+func getRequest(db *sql.DB, c *gin.Context) {
+	tinyUrl := c.Params.ByName("tinyurl")
+	id, err := TinyUrlToId(tinyUrl)
+	if err != nil {
+		c.Writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	var longUrl string
+	err = db.QueryRow(findById, id).Scan(&longUrl)
+	if err != nil {
+		c.Writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	c.Redirect(http.StatusFound, longUrl)
+}
+
+func SetupRouter(db *sql.DB) *gin.Engine {
+	r := gin.Default()
+
+	r.PUT("/create", func(c *gin.Context) {
+		putRequest(db, c)
+	})
+
+	r.GET("/:tinyurl", func(c *gin.Context) {
+		getRequest(db, c)
+	})
+
+	return r
+}
---
+++ homework-01/url_functions.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+const alphabet = 62
+const maxLen = 7
+
+func IdToTinyUrl(id int) (string, error) {
+	if id < 0 || id >= alphabet^maxLen {
+		return "", fmt.Errorf("wrong id")
+	}
+
+	bytes := make([]byte, 7)
+
+	for i := 0; i < maxLen; i++ {
+		bytes[i] = intToByte(id % alphabet)
+	}
+
+	return string(bytes), nil
+}
+
+func intToByte(id int) byte {
+	if id < 10 {
+		return byte('0' + id)
+	}
+
+	if id >= 10 && id < 36 {
+		return byte('a' + (id - 10))
+	}
+
+	return byte('A' + (id - 36))
+}
+
+func TinyUrlToId(url string) (int, error) {
+	if len(url) != maxLen {
+		return 0, fmt.Errorf("wrong url size")
+	}
+
+	bytes := []byte(url)
+	id := 0
+
+	for i := 0; i < maxLen; i++ {
+		id *= alphabet
+		char, err := byteToInt(bytes[maxLen-i-1])
+		if err != nil {
+			return 0, err
+		}
+
+		id += char
+	}
+
+	return id, nil
+}
+
+func byteToInt(b byte) (int, error) {
+	if b >= 'a' && b <= 'z' {
+		return int(b - 'a'), nil
+	}
+
+	if b >= 'A' && b <= 'Z' {
+		return int(b - 'A' + 26), nil
+	}
+
+	if b >= '0' && b <= '9' {
+		return int(b - '0' + 52), nil
+	}
+
+	return 0, fmt.Errorf("wrong symbol")
+}
