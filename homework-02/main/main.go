804354989035dc6363c079b9d8acbcb48b3892d9
Anaiya798 <persikk3000@gmail.com>
2022-11-23 21:04:41 +0300

    add run example

--- homework-02/main/main.go
+++ homework-02/main/main.go
@@ -7,16 +7,29 @@ import (
 )
 
 func main() {
+	/*c, _, err := zk.Connect([]string{"84.201.138.191:2181"}, time.Minute) //*10)
+	if err != nil {
+		panic(err)
+	}
+	_, error := c.Create("/aisakova/dqueues/test", []byte("test"), 0, nil)
+	fmt.Println(error)*/
 	redisOptions := redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	}
 
-	hosts := []string{"localhost:6379"}
+	//hosts := []string{"localhost:7000", "localhost:7001", "localhost:7002", "localhost:7003"}
+	hosts := []string{"158.160.9.8:6379", "51.250.106.140:6379", "158.160.19.212:6379", "158.160.19.2:6379"}
+	//hosts := []string{"localhost:6379"}
 	dqueue.Config(&redisOptions)
-	firstD, _ := dqueue.Open("first", 1, &hosts)
-	firstD.Push("bbb")
+	firstD, _ := dqueue.Open("first", 4, &hosts)
+	firstD.Push("aaa1")
+	firstD.Push("aaa2")
+	firstD.Push("aaa3")
+	firstD.Push("aaa4")
+	firstD.Push("aaa5")
+
 	firstD.Pull()
 
 }
