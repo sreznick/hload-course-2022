6202ad782d3027165cadfe328b874a61eb5dfece
Vyacheslav Tamarin <vyacheslav.tamarin@yandex.ru>
2022-11-26 18:58:53 +0300

    Add Homework-02

--- homework-02/dqueue/dqueue.go
+++ homework-02/dqueue/dqueue.go
@@ -1,29 +1,38 @@
 package dqueue
 
 import (
-//    "fmt"
-//    "context"
-
-    "github.com/go-redis/redis/v8"
-//    "github.com/go-zookeeper/zk"
-
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/go-zookeeper/zk"
+	"sort"
+	"strconv"
+	"time"
 )
 
 /*
  * Можно создавать несколько очередей
  *
  * Для клиента они различаются именами
- * 
+ *
  * В реализации они могут потребовать вспомогательных данных
- * Для них - эта структура. Можете определить в ней любые поля 
+ * Для них - эта структура. Можете определить в ней любые поля
  */
 type DQueue struct {
+	name         string
+	nShards      int
+	currentIndex int
+	prefix       string
 }
+
+var ctx = context.Background()
 
 /*
  * Запомнить данные и везде использовать
  */
-func Config(redisOptions *redis.Options, zkCluster []string) {
+func Config(redisAddrs []string, zkCluster []string) {
+	ctx = context.WithValue(ctx, "redisAddrs", redisAddrs)
+	ctx = context.WithValue(ctx, "zkCluster", zkCluster)
 }
 
 /*
@@ -40,28 +49,208 @@ func Config(redisOptions *redis.Options, zkCluster []string) {
  *
  * Отдельные узлы Redis-кластера могут выпадать. Availability очереди в целом
  * не должна от этого страдать
- *  
+ *
  */
 func Open(name string, nShards int) (DQueue, error) {
-    return DQueue{}, nil
+	prefix := "tamarinvs"
+	zkAddr := ctx.Value("zkCluster")
+	switch v := zkAddr.(type) {
+	case []string:
+		c, _, err := zk.Connect(v, time.Second) //*10)
+		if err != nil {
+			panic(err)
+		}
+		createZkRoot(c, prefix, name)
+		infoPath := fmt.Sprintf("/%s_info_%s", prefix, name)
+		info, _, err := c.Get(infoPath)
+		if err != nil {
+			_, err := c.Create(infoPath, []byte(strconv.Itoa(nShards)), 0, zk.WorldACL(zk.PermAll))
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			infoShards, err := strconv.Atoi(string(info)[:])
+			if err != nil {
+				panic(err)
+			}
+			if infoShards != nShards {
+				return DQueue{}, fmt.Errorf("invalid number of shards %d. Expected %d", nShards, infoShards)
+			}
+		}
+	}
+	return DQueue{name, nShards, 0, prefix}, nil
 }
 
 /*
  * Пишем в очередь. Каждый следующий Push - в следующий шард
- * 
+ *
  * Если шард упал - пропускаем шард, пишем в следующий по очереди
  */
-func (*DQueue) Push(value string) error {
-    return nil
+func (dq *DQueue) Push(value string) error {
+	redisAddrs := ctx.Value("redisAddrs")
+	switch v := redisAddrs.(type) {
+	case []string:
+		for {
+			alive := checkRedisAddress(dq.currentIndex)
+			if alive {
+				redisOptions := redis.Options{
+					Addr:     v[dq.currentIndex],
+					Password: "",
+					DB:       0,
+				}
+				client := redis.NewClient(&redisOptions)
+				client.RPush(client.Context(), fmt.Sprintf("%s::%s", dq.prefix, dq.name), value)
+
+				err := SaveToZk(dq.prefix, dq.name, strconv.Itoa(dq.currentIndex))
+				return err
+			} else {
+				dq.currentIndex = (dq.currentIndex) % dq.nShards
+			}
+		}
+	}
+	return nil
 }
 
 /*
  * Читаем из очереди
  *
  * Из того шарда, в котором самое раннее сообщение
- * 
+ *
  */
-func (*DQueue) Pull() (string, error) {
-    return "", nil
+func (dq *DQueue) Pull() (string, error) {
+	redisAddrs := ctx.Value("redisAddrs")
+	switch v := redisAddrs.(type) {
+	case []string:
+		redisAddr := getNextAddress(dq.prefix, dq.name)
+		redisOptions := redis.Options{
+			Addr:     v[redisAddr],
+			Password: "",
+			DB:       0,
+		}
+		client := redis.NewClient(&redisOptions)
+		return client.LPop(client.Context(), fmt.Sprintf("%s::%s", dq.prefix, dq.name)).Val(), nil
+	}
+	return "", nil
 }
 
+func SaveToZk(prefix string, queueName string, redisAddr string) error {
+	zkAddr := ctx.Value("zkCluster")
+	switch v := zkAddr.(type) {
+	case []string:
+		c, _, err := zk.Connect(v, time.Second) //*10)
+		if err != nil {
+			panic(err)
+		}
+
+		pathToRedis := fmt.Sprintf("/%s/%s", prefix, queueName)
+
+		children, _, _, err := c.ChildrenW(pathToRedis)
+		index := -1
+		if err == nil {
+			for _, child := range children {
+				childId, errParse := strconv.Atoi(child)
+				if errParse != nil {
+					panic(errParse)
+				}
+				if -index > -childId {
+					index = childId
+				}
+			}
+		}
+		index += 1
+
+		path := fmt.Sprintf("/%s/%s/%d", prefix, queueName, index)
+		_, errCreate := c.Create(path, []byte(redisAddr), 0, zk.WorldACL(zk.PermAll))
+		if errCreate != nil {
+			panic(errCreate)
+		}
+	}
+	return nil
+}
+
+func createZkRoot(c *zk.Conn, prefix string, queueName string) {
+	path1 := fmt.Sprintf("/%s/%s", prefix, queueName)
+	path2 := fmt.Sprintf("/%s", prefix)
+
+	exists, _, err := c.Exists(path2)
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		_, errCreate := c.Create(path2, []byte{}, 0, zk.WorldACL(zk.PermAll))
+		if errCreate != nil {
+			panic(errCreate)
+		}
+	}
+	exists, _, err = c.Exists(path1)
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		_, errCreate := c.Create(path1, []byte{}, 0, zk.WorldACL(zk.PermAll))
+		if errCreate != nil {
+			panic(errCreate)
+		}
+	}
+}
+
+func getNextAddress(prefix string, queueName string) int {
+	zkAddr := ctx.Value("zkCluster")
+	switch v := zkAddr.(type) {
+	case []string:
+		c, _, err := zk.Connect(v, time.Second) //*10)
+		if err != nil {
+			panic(err)
+		}
+
+		createZkRoot(c, prefix, queueName)
+		pathToRedis := fmt.Sprintf("/%s/%s", prefix, queueName)
+
+		children, _, _, err := c.ChildrenW(pathToRedis)
+		if err != nil {
+			panic(err)
+		}
+		childrenIds := make([]int, len(children))
+		for i, child := range children {
+			childId, errParse := strconv.Atoi(child)
+			if errParse != nil {
+				panic(errParse)
+			}
+			childrenIds[i] = childId
+		}
+		childrenSlice := childrenIds[:]
+		sort.Ints(childrenSlice)
+		for _, child := range childrenIds {
+			path := fmt.Sprintf("%s/%d", pathToRedis, child)
+			redisAddrBytes, _, err2 := c.Get(path)
+			if err2 != nil {
+				panic(err2)
+			}
+			err := c.Delete(path, -1)
+			if err != nil {
+				return 0
+			}
+			redisAddr, _ := strconv.Atoi(string(redisAddrBytes[:]))
+			if checkRedisAddress(redisAddr) {
+				return redisAddr
+			}
+		}
+	}
+	return 0
+}
+
+func checkRedisAddress(addrIndex int) bool {
+	zkAddr := ctx.Value("zkCluster")
+	switch v := zkAddr.(type) {
+	case []string:
+		client := redis.NewClient(&redis.Options{
+			Addr:     v[addrIndex],
+			Password: "",
+			DB:       0,
+		})
+
+		_, err := client.Ping(client.Context()).Result()
+		return err != nil
+	}
+	return false
+}
--- homework-02/main/main.go
+++ homework-02/main/main.go
@@ -1,19 +1,65 @@
 package main
 
 import (
-//    "fmt"
-    "dqueue"
-//    "context"
-
-    "github.com/go-redis/redis/v8"
+	"dqueue"
+	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
-func main() {
-    redisOptions := redis.Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-    }
+/*
+Read files .redisAddr and .zkAddr than split it by \n.
+*/
+func readConfig() ([]string, []string) {
+	contentRedis, err := ioutil.ReadFile(".redisAddr")
+	if err != nil {
+		panic(err)
+	}
+	contentZk, err := ioutil.ReadFile(".zkAddr")
+	if err != nil {
+		panic(err)
+	}
+	return strings.Split(string(contentRedis), "\n"), strings.Split(string(contentZk), "\n")
+}
 
-    dqueue.Config(&redisOptions, []string{"127.0.0.1"})
+func main() {
+	redisAddr, zkAddr := readConfig()
+	dqueue.Config(redisAddr, zkAddr)
+
+	dq, err := dqueue.Open("test", 1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	errPush := dq.Push("123")
+	if errPush != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Push %d\n", 123)
+	errPush = dq.Push("456")
+	if errPush != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Push %d\n", 789)
+	errPush = dq.Push("789")
+	if errPush != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Push %d\n", 465)
+
+	pullData, err := dq.Pull()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Pull %s\n", pullData)
+	pullData, err = dq.Pull()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Pull %s\n", pullData)
 }
