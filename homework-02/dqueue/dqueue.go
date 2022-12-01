96e4b0af5923b9f9dac407607a619665d1060629
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2022-12-01 12:18:08 +0300

    Init Push

--- homework-02/dqueue/dqueue.go
+++ homework-02/dqueue/dqueue.go
@@ -1,67 +1,389 @@
 package dqueue
 
 import (
-//    "fmt"
-//    "context"
-
-    "github.com/go-redis/redis/v8"
-//    "github.com/go-zookeeper/zk"
-
+	"context"
+	"errors"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/go-zookeeper/zk"
+	"sort"
+	"strconv"
+	"time"
 )
 
-/*
- * Можно создавать несколько очередей
- *
- * Для клиента они различаются именами
- * 
- * В реализации они могут потребовать вспомогательных данных
- * Для них - эта структура. Можете определить в ней любые поля 
- */
+const prefix = "bmadzhuga"
+const limit = 1000
+
 type DQueue struct {
+	name     string
+	nShards  int
+	curIndex int
 }
 
-/*
- * Запомнить данные и везде использовать
- */
-func Config(redisOptions *redis.Options, zkCluster []string) {
+var config map[string][]string
+
+func Config(redisCluster []string, zkCluster []string) {
+	config = make(map[string][]string, 2)
+	config["redisCluster"] = redisCluster
+	config["zkCluster"] = zkCluster
 }
 
-/*
- * Открываем очередь на nShards шардах
- *
- * Попытка создать очередь с существующим именем и другим количеством шардов
- * должна приводить к ошибке
- *
- * При попытке создать очередь с существующим именем и тем же количеством шардов
- * нужно вернуть экземпляр DQueue, позволяющий делать Push/Pull
- *
- * Предыдущее открытие может быть совершено другим клиентом, соединенным с любым узлом
- * Redis-кластера
- *
- * Отдельные узлы Redis-кластера могут выпадать. Availability очереди в целом
- * не должна от этого страдать
- *  
- */
+func createZNode(connect *zk.Conn, infoPath string, data []byte) error {
+	_, err := connect.Create(
+		infoPath,
+		data,
+		0,
+		zk.WorldACL(zk.PermAll),
+	)
+
+	if err != nil {
+		return fmt.Errorf("Zookeeper node creation failed: %w", err)
+	}
+
+	return nil
+}
+
+func getIndex(connect *zk.Conn, queueName string) (int, error) {
+	pathToRedis := fmt.Sprintf("/%s/%s", prefix, queueName)
+	children, _, _, err := connect.ChildrenW(pathToRedis)
+	if err != nil {
+		return 0, nil
+	}
+
+	var (
+		index   = -1
+		childID int
+	)
+	for _, child := range children {
+		childID, err = strconv.Atoi(child)
+		if err != nil {
+			return 0, fmt.Errorf("Can't parse childID to int: %w", err)
+		}
+
+		if index <= childID {
+			index = childID
+		}
+	}
+
+	return index + 1, nil
+}
+
+func createZNodeWithIndex(queueName string, redisAddr string) error {
+	zkCluster, ok := config["zkCluster"]
+	if !ok {
+		return fmt.Errorf("Zookeeper cluster isn't found")
+	}
+
+	connect, _, err := zk.Connect(zkCluster, time.Second)
+	if err != nil {
+		return fmt.Errorf("Zookeeper connection failed: %w", err)
+	}
+
+	index, err := getIndex(connect, queueName)
+	if err != nil {
+		return fmt.Errorf("Getting index failed: %w", err)
+	}
+
+	path := fmt.Sprintf("/%s/%s/%d", prefix, queueName, index)
+	return createZNode(connect, path, []byte(redisAddr))
+}
+
+func createZNodeRoot(connect *zk.Conn, queueName string) error {
+	err := checkExistsAndCreateZNode(connect, fmt.Sprintf("/%s", prefix))
+	if err != nil {
+		return fmt.Errorf("Checking node failed: %w", err)
+	}
+
+	err = checkExistsAndCreateZNode(connect, fmt.Sprintf("/%s/%s", prefix, queueName))
+	if err != nil {
+		return fmt.Errorf("Checking and creating node failed: %w", err)
+	}
+
+	return nil
+}
+
+func checkExistsAndCreateZNode(connect *zk.Conn, path string) error {
+	exists, _, err := connect.Exists(path)
+	if err != nil {
+		return fmt.Errorf("Path %v doesn't exist: %w", path, err)
+	}
+
+	if exists {
+		return nil
+	}
+
+	return createZNode(connect, path, []byte{})
+}
+
+func checkAddrToRedis(addrIndex int) error {
+	redisAddr, ok := config["redisCluster"]
+	if !ok {
+		return errors.New("Can't read Redis config")
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisAddr[addrIndex],
+		Password: "",
+		DB:       0,
+	})
+
+	_, err := client.Ping(client.Context()).Result()
+
+	return err
+}
+
+func nextAddress(queueName string) (int, error) {
+	zkAddr, ok := config["zkCluster"]
+	if !ok {
+		return 0, fmt.Errorf("Zookeeper cluster isn't found")
+	}
+
+	connect, _, err := zk.Connect(zkAddr, time.Second)
+	if err != nil {
+		return 0, fmt.Errorf("Zookeeper connect failed: %w", err)
+	}
+
+	err = createZNodeRoot(connect, queueName)
+	if err != nil {
+		return 0, fmt.Errorf("Creation Zookeeper node failed: %w", err)
+	}
+
+	pathToRedis := fmt.Sprintf("/%s/%s", prefix, queueName)
+	children, _, _, err := connect.ChildrenW(pathToRedis)
+	if err != nil {
+		return 0, fmt.Errorf("Connection children failed: %w", err)
+	}
+
+	var (
+		childID     int
+		childrenIDs = make([]int, 0, len(children))
+	)
+	for _, child := range children {
+		childID, err = strconv.Atoi(child)
+		if err != nil {
+			return 0, fmt.Errorf("Can't parse childID to id: %w", err)
+		}
+
+		childrenIDs = append(childrenIDs, childID)
+	}
+	sort.Ints(childrenIDs)
+
+	var (
+		redisBytes  []byte
+		addrToRedis int
+	)
+	for _, child := range childrenIDs {
+		path := fmt.Sprintf("%s/%d", pathToRedis, child)
+
+		redisBytes, _, err = connect.Get(path)
+		if err != nil {
+			return 0, fmt.Errorf("Connection get failed: %w", err)
+		}
+
+		err = connect.Delete(path, -1)
+		if err != nil {
+			return 0, fmt.Errorf("Connection delete failed: %w", err)
+		}
+
+		addrToRedis, err = strconv.Atoi(string(redisBytes))
+		if err != nil {
+			return 0, fmt.Errorf("Can't parse redisBytes to int: %w", err)
+		}
+
+		err := checkAddrToRedis(addrToRedis)
+		if err == nil {
+			return addrToRedis, nil
+		}
+	}
+
+	return 0, nil
+
+}
+
 func Open(name string, nShards int) (DQueue, error) {
-    return DQueue{}, nil
+
+	dQueue := DQueue{
+		name:     name,
+		nShards:  nShards,
+		curIndex: 0,
+	}
+
+	zkCluster, ok := config["zkCluster"]
+	if !ok {
+		return DQueue{}, fmt.Errorf("Can't find Zookeeper cluster in config")
+	}
+
+	connect, _, err := zk.Connect(zkCluster, time.Second)
+	if err != nil {
+		return DQueue{}, fmt.Errorf("Zookeeper connection failed: %w", err)
+	}
+
+	if err = createZNodeRoot(connect, name); err != nil {
+		return DQueue{}, fmt.Errorf("Zookeeper root node creation failed: %w", err)
+	}
+
+	infoPath := fmt.Sprintf("/%v_info_%v", prefix, name)
+	infoBytes, _, err := connect.Get(infoPath)
+
+	if err != nil {
+		return dQueue, createZNode(connect, infoPath, []byte(fmt.Sprintf("%v", nShards)))
+	}
+
+	infoShards, err := strconv.Atoi(string(infoBytes))
+	if err != nil {
+		return DQueue{}, fmt.Errorf("Convertation bytes to int failed: %w", err)
+	}
+
+	if infoShards != nShards {
+		return DQueue{}, fmt.Errorf("Info shards amount (%d) doesn't equal to shards amount (%d)", nShards, infoShards)
+	}
+
+	return dQueue, nil
 }
 
-/*
- * Пишем в очередь. Каждый следующий Push - в следующий шард
- * 
- * Если шард упал - пропускаем шард, пишем в следующий по очереди
- */
-func (*DQueue) Push(value string) error {
-    return nil
+func (d *DQueue) Push(value string) error {
+	redisCluster, ok := config["redisCluster"]
+	if !ok {
+		return fmt.Errorf("Can't find redis cluster")
+	}
+
+	counter := 0
+
+	for {
+		err := checkAddrToRedis(d.curIndex)
+		if err != nil {
+			counter++
+			d.curIndex = (d.curIndex) % d.nShards
+
+			if counter > limit {
+				return err
+			}
+
+			continue
+		}
+
+		addr := redisCluster[d.curIndex]
+
+		redisOptions := redis.Options{
+			Addr:     addr,
+			Password: "",
+			DB:       0,
+		}
+
+		key, err := d.findKeyForAddr(addr)
+
+		if err != nil {
+			return fmt.Errorf("Can't find key for slot: %w", err)
+		}
+
+		client := redis.NewClient(&redisOptions)
+		client.RPush(client.Context(), key, value)
+
+		return createZNodeWithIndex(d.name, strconv.Itoa(d.curIndex))
+	}
 }
 
-/*
- * Читаем из очереди
- *
- * Из того шарда, в котором самое раннее сообщение
- * 
- */
-func (*DQueue) Pull() (string, error) {
-    return "", nil
+func (d *DQueue) Pull() (string, error) {
+	redisCluster, ok := config["redisCluster"]
+	if !ok {
+		return "", fmt.Errorf("Redis cluster isn't found")
+	}
+
+	redisAddr, err := nextAddress(d.name)
+	if err != nil {
+		return "", fmt.Errorf("Getting next adress failed: %w", err)
+	}
+
+	addr := redisCluster[redisAddr]
+
+	fmt.Println(addr)
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "",
+		DB:       0,
+	})
+
+	key, err := d.findKeyForAddr(addr)
+
+	if err != nil {
+		return "", fmt.Errorf("Can't find key for slot: %w", err)
+	}
+
+	res, err := client.LPop(client.Context(), key).Result()
+
+	if err != nil {
+		return "", fmt.Errorf("Can't pop value: %w", err)
+	}
+
+	return res, nil
 }
 
+func (d *DQueue) findKeyForAddr(addr string) (string, error) {
+	redisCluster, ok := config["redisCluster"]
+	if !ok {
+		return "", fmt.Errorf("Redis cluster isn't found")
+	}
+
+	redisAddr, err := nextAddress(d.name)
+	if err != nil {
+		return "", fmt.Errorf("Getting next adress failed: %w", err)
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisCluster[redisAddr],
+		Password: "",
+		DB:       0,
+	})
+
+	ctx := context.Background()
+
+	slots, err := client.ClusterSlots(ctx).Result()
+
+	if err != nil {
+		return "", fmt.Errorf("Getting cluster slots failed: %w", err)
+	}
+
+	left := 0
+	right := 16000
+	flag := false
+
+	for i := range slots {
+		slot := slots[i]
+		nodes := slot.Nodes
+		for k := range nodes {
+			node := nodes[k]
+			if node.Addr == addr {
+				left = slot.Start
+				right = slot.End
+				flag = true
+				break
+			}
+
+			if node.Addr == "10.129.0.33:6379" && addr == "158.160.9.8:6379" {
+				left = slot.Start
+				right = slot.End
+				flag = true
+				break
+			}
+		}
+		if flag {
+			break
+		}
+	}
+
+	if !flag {
+		return "", errors.New("Can't find target cluster")
+	}
+
+	counter := 0
+
+	for {
+		key := fmt.Sprintf("%s::%s::%v", prefix, d.name, counter)
+		randSlot := int(client.ClusterKeySlot(ctx, key).Val())
+
+		if left < randSlot && randSlot < right {
+			return key, nil
+		}
+	}
+}
--- homework-02/main/main.go
+++ homework-02/main/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
-//    "fmt"
-    "dqueue"
-//    "context"
-
-    "github.com/go-redis/redis/v8"
+	"dqueue"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 func main() {
-    redisOptions := redis.Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-    }
 
-    dqueue.Config(&redisOptions, []string{"127.0.0.1"})
+	redis := []string{"158.160.9.8:6379", "51.250.106.140:6379", "158.160.19.212:6379", "158.160.19.2:6379"}
+	zookeeper := []string{"158.160.9.8", "51.250.106.140", "158.160.19.212", "158.160.19.2"}
+
+	dqueue.Config(redis, zookeeper)
+
+	rand.Seed(time.Now().UnixNano())
+
+	dqueue, err := dqueue.Open("main", 4)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := 0; i < 10; i++ {
+		data := strconv.Itoa(rand.Intn(100))
+		err := dqueue.Push(data)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		data, err := dqueue.Pull()
+		if err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			fmt.Println(data)
+		}
+	}
+
 }
