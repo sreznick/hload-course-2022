67c18bae4e831b2d241316cbb3470c9e6c62a389
Daniil Kulikov <kuldanvit@gmail.com>
2022-12-02 01:23:59 +0300

    Revert "+hw"
    This reverts commit 5b3fe12e7af67e9c7bb6d7784fd65e7fd72a8caf.

--- homework-02/dqueue/dqueue.go
+++ homework-02/dqueue/dqueue.go
@@ -1,61 +1,33 @@
 package dqueue
 
 import (
-	//    "fmt"
-	//    "context"
+//    "fmt"
+//    "context"
 
-	"context"
-	"encoding/json"
-	"fmt"
-	"github.com/go-redis/redis/v8"
-	"github.com/go-zookeeper/zk"
-	"strconv"
-	"strings"
-	"time"
-	//    "github.com/go-zookeeper/zk"
+    "github.com/go-redis/redis/v8"
+//    "github.com/go-zookeeper/zk"
+
 )
-
-var cluster RedisCluster
-var _zkHosts []string
-
-const (
-	queuesPath = "/dkulikov/queues"
-	letters    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-)
-
-type LocalQueue struct {
-	Name string
-	Host string
-}
 
 /*
  * Можно создавать несколько очередей
  *
  * Для клиента они различаются именами
- *
+ * 
  * В реализации они могут потребовать вспомогательных данных
- * Для них - эта структура. Можете определить в ней любые поля
+ * Для них - эта структура. Можете определить в ней любые поля 
  */
 type DQueue struct {
-	Name       string
-	NShards    int
-	RedisHosts []LocalQueue
-
-	CurIdPush int
-	CurIdPop  int
-	Version   int32
 }
 
 /*
  * Запомнить данные и везде использовать
  */
-func Config(redisData RedisCluster, zkCluster []string) {
-	cluster = redisData
-	_zkHosts = zkCluster
+func Config(redisOptions *redis.Options, zkCluster []string) {
 }
 
 /*
- * Открываем очередь на NShards шардах
+ * Открываем очередь на nShards шардах
  *
  * Попытка создать очередь с существующим именем и другим количеством шардов
  * должна приводить к ошибке
@@ -68,291 +40,28 @@ func Config(redisData RedisCluster, zkCluster []string) {
  *
  * Отдельные узлы Redis-кластера могут выпадать. Availability очереди в целом
  * не должна от этого страдать
- *
+ *  
  */
 func Open(name string, nShards int) (DQueue, error) {
-	c, _, err := zk.Connect(_zkHosts, time.Second*3)
-	if err != nil {
-		return DQueue{}, err
-	}
-	defer c.Close()
-
-	s, q, err := handleExisting(c, name, nShards)
-	if err != nil {
-		return DQueue{}, err
-	}
-	if s {
-		return q, nil
-	}
-
-	// Locks zk
-	err = Lock(c, queuesPath)
-	if err != nil {
-		return DQueue{}, err
-	}
-
-	// If created after lock
-	s, q, err = handleExisting(c, name, nShards)
-	if err != nil {
-		return DQueue{}, err
-	}
-	if s {
-		return q, nil
-	}
-
-	queuePath := queuesPath + "/" + name
-	return createQueueZKNode(c, queuePath, nShards, name)
-}
-
-func handleExisting(c *zk.Conn, name string, nShards int) (bool, DQueue, error) {
-	// Getting info about queue if exists
-	queuePath := queuesPath + "/" + name
-	ex, _, err := c.Exists(queuePath)
-	if err != nil {
-		return false, DQueue{}, err
-	}
-	if ex {
-		q, err := getQueueInfo(c, queuePath)
-		if err != nil {
-			return true, DQueue{}, err
-		}
-
-		if q.NShards != nShards {
-			return true, DQueue{}, fmt.Errorf("Conflict nshards with existing queue")
-		}
-
-		return true, q, nil
-	}
-
-	return false, DQueue{}, nil
-}
-
-func getQueueInfo(c *zk.Conn, queuePath string) (DQueue, error) {
-	s, _, err := c.Get(queuePath)
-	if err != nil {
-		return DQueue{}, err
-	}
-	q := DQueue{}
-	err = json.Unmarshal(s, &q)
-	if err != nil {
-		return DQueue{}, err
-	}
-
-	return q, nil
-}
-
-func getAllStringsLengthKBack(k int, s string) []string {
-	strings := []string{}
-	if k == 0 {
-		return []string{s}
-	}
-
-	for i := 0; i < len(letters); i++ {
-		strings = append(strings, getAllStringsLengthKBack(k-1, s+string(letters[i]))...)
-	}
-
-	return strings
-}
-
-func getAllStringsLengthK(k int) []string {
-	return getAllStringsLengthKBack(k, "")
-}
-
-func contains(rs []LocalQueue, r redis.Options) bool {
-	for _, rr := range rs {
-		if rr.Host == r.Addr {
-			return true
-		}
-	}
-
-	return false
-}
-
-func selectHosts(n int, outerName string) ([]LocalQueue, error) {
-	length := 1
-	var hosts []LocalQueue
-	for {
-		strings := getAllStringsLengthK(length)
-		for _, s := range strings {
-			s = "dkulikov:" + outerName + ":" + s
-			host, err := cluster.get_key_host(s)
-			if err != nil {
-				continue
-			}
-
-			if contains(hosts, host) {
-				continue
-			}
-			hosts = append(hosts, LocalQueue{Host: host.Addr, Name: s})
-			if len(hosts) == n {
-				return hosts, nil
-			}
-		}
-		length++
-		if length >= 10000 {
-			return hosts, fmt.Errorf("No suitable hosts")
-		}
-	}
-}
-
-func createQueueZKNode(c *zk.Conn, queuePath string, nShards int, name string) (DQueue, error) {
-	redisHosts, err := selectHosts(nShards, name)
-	if err != nil {
-		return DQueue{}, nil
-	}
-	q := DQueue{
-		NShards:    nShards,
-		Name:       name,
-		RedisHosts: redisHosts,
-
-		CurIdPush: 0,
-		CurIdPop:  0,
-		Version:   0,
-	}
-
-	js, err := json.Marshal(q)
-	if err != nil {
-		return DQueue{}, err
-	}
-	_, err = c.Create(queuePath, js, 0, zk.WorldACL(zk.PermAll))
-	if err != nil {
-		return DQueue{}, err
-	}
-
-	_, err = c.Create(queuePath+"/_locknode", []byte{}, 0, zk.WorldACL(zk.PermAll))
-
-	return q, nil
+    return DQueue{}, nil
 }
 
 /*
  * Пишем в очередь. Каждый следующий Push - в следующий шард
- *
+ * 
  * Если шард упал - пропускаем шард, пишем в следующий по очереди
  */
-func (q *DQueue) Push(value string) error {
-	c, _, err := zk.Connect(_zkHosts, time.Second*3)
-	if err != nil {
-		return err
-	}
-	defer c.Close()
-
-	queuePath := queuesPath + "/" + q.Name
-	err = Lock(c, queuePath)
-	if err != nil {
-		return err
-	}
-
-	s, st, err := c.Get(queuePath)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(s, q)
-	if err != nil {
-		return err
-	}
-	q.Version = st.Version
-
-	for {
-		redisQueue := q.RedisHosts[q.CurIdPush%q.NShards]
-		ro := redis.Options{Addr: redisQueue.Host, Password: "", DB: 0}
-		q.CurIdPush++
-
-		ctx := context.Background()
-		r := redis.NewClient(&ro)
-
-		_, err = r.RPush(ctx, redisQueue.Name, fmt.Sprintf("%s|%d", value, q.CurIdPush)).Result()
-		if err != nil {
-			continue
-		}
-		break
-	}
-
-	self, err := json.Marshal(q)
-	if err != nil {
-		return err
-	}
-
-	_, err = c.Set(queuePath, self, q.Version)
-	if err != nil {
-		return err
-	}
-
-	return nil
+func (*DQueue) Push(value string) error {
+    return nil
 }
 
 /*
  * Читаем из очереди
  *
  * Из того шарда, в котором самое раннее сообщение
- *
+ * 
  */
-func (q *DQueue) Pull() (string, error) {
-	c, _, err := zk.Connect(_zkHosts, time.Second*3)
-	if err != nil {
-		return "", err
-	}
-	defer c.Close()
-
-	queuePath := queuesPath + "/" + q.Name
-	err = Lock(c, queuePath)
-	if err != nil {
-		return "", err
-	}
-
-	s, st, err := c.Get(queuePath)
-	if err != nil {
-		return "", err
-	}
-
-	err = json.Unmarshal(s, q)
-	if err != nil {
-		return "", err
-	}
-	q.Version = st.Version
-
-	for {
-		redisQueue := q.RedisHosts[q.CurIdPop%q.NShards]
-		ro := redis.Options{Addr: redisQueue.Host, Password: "", DB: 0}
-
-		ctx := context.Background()
-		r := redis.NewClient(&ro)
-
-		v, err := r.LIndex(ctx, redisQueue.Name, 0).Result()
-		if err != nil {
-			q.CurIdPop++
-			continue
-		}
-
-		strs := strings.Split(v, "|")
-		id, err := strconv.Atoi(strs[1])
-		v = strs[0]
-		if err != nil {
-			q.CurIdPop++
-			continue
-		}
-
-		if id > q.CurIdPop {
-			q.CurIdPop++
-			continue
-		}
-
-		_, err = r.LPop(ctx, redisQueue.Name).Result()
-		if err != nil {
-			q.CurIdPop++
-			continue
-		}
-
-		self, err := json.Marshal(q)
-		if err != nil {
-			return "", err
-		}
-
-		_, err = c.Set(queuePath, self, q.Version)
-		if err != nil {
-			return "", err
-		}
-
-		return v, nil
-	}
+func (*DQueue) Pull() (string, error) {
+    return "", nil
 }
+
--- homework-02/main/main.go
+++ homework-02/main/main.go
@@ -1,103 +1,19 @@
 package main
 
 import (
-	"dqueue"
-	"fmt"
-	"reflect"
-	"sort"
-	"strconv"
-	"sync"
+//    "fmt"
+    "dqueue"
+//    "context"
+
+    "github.com/go-redis/redis/v8"
 )
 
-func test_queue(name string) {
-	q, err := dqueue.Open(name, 2)
-	if err != nil {
-		fmt.Println(name)
-		panic(err)
-	}
-
-	var pushed []string
-	for i := 0; i < 3; i++ {
-		msg := "Hi " + strconv.Itoa(i)
-		err = q.Push(msg)
-		if err != nil {
-			panic(err)
-		}
-		pushed = append(pushed, msg)
-	}
-
-	var pulled []string
-	for i := 0; i < 3; i++ {
-		v, err := q.Pull()
-		if err != nil {
-			panic(err)
-		}
-		pulled = append(pulled, v)
-	}
-
-	sort.Strings(pulled)
-	sort.Strings(pushed)
-	if !reflect.DeepEqual(pulled, pushed) {
-		panic(fmt.Sprintf("!ACHTUNG! %s\n", name))
-	}
-}
-
-func test_one_queue(name string) {
-	q, err := dqueue.Open(name, 4)
-	if err != nil {
-		fmt.Println(name)
-		panic(err)
-	}
-
-	for i := 0; i < 3; i++ {
-		msg := "Hi " + strconv.Itoa(i)
-		err = q.Push(msg)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	for i := 0; i < 3; i++ {
-		_, err := q.Pull()
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
 func main() {
-	r, z, err := ReadConfig()
-	dqueue.Config(r, z)
-	if err != nil {
-		panic(err)
-	}
+    redisOptions := redis.Options{
+        Addr:     "localhost:6379",
+        Password: "", // no password set
+        DB:       0,  // use default DB
+    }
 
-	var wg sync.WaitGroup
-	{
-		for i := 0; i < 6; i++ {
-			wg.Add(1)
-			i_ := i + 1
-			go func() {
-				test_queue("small_queue" + strconv.Itoa(i_))
-				wg.Done()
-			}()
-		}
-
-		wg.Wait()
-		fmt.Println("Group 1 done")
-	}
-
-	{
-		for i := 0; i < 6; i++ {
-			wg.Add(1)
-			go func() {
-				test_one_queue("big_queue")
-				wg.Done()
-			}()
-		}
-
-		wg.Wait()
-		fmt.Println("Group 2 done")
-	}
-
+    dqueue.Config(&redisOptions, []string{"127.0.0.1"})
 }
