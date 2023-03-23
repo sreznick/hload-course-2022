0a0f4694f0dfc0a38effaee2f1bce952529c268d
Margarita Lashina <lashina.2000@gmail.com>
2023-03-23 17:10:28 +0300

    added task2

--- homework-01/main.go
+++ homework-01/main.go
@@ -6,8 +6,12 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 const SQL_DRIVER = "postgres"
@@ -30,16 +34,38 @@ func GetTinyUrlById(id int) string {
 	return str
 }
 
-func GetIdByTinyUrl(tinyurl string) int {
+func GetIdByTinyUrl(tinyurl string, c *gin.Context) int {
 	data, err := base32.StdEncoding.WithPadding(-1).DecodeString(tinyurl)
 	ErrorCheck(err, "Decode error:")
-	fmt.Println(tinyurl)
-	fmt.Println(string(data[:]))
-	fmt.Println(tinyurl)
 	id, err := strconv.Atoi(string(data[:]))
-	ErrorCheck(err, "Decode error while converting:")
+	if err != nil {
+		c.Writer.WriteHeader(404)
+		return -1
+	}
 	return id
 }
+
+var (
+	createRequestsNumber = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "Put_requests",
+		Help: "Number of /create PUT requests",
+	})
+
+	createRequestTime = promauto.NewSummary(prometheus.SummaryOpts{
+		Name: "Put_request_time",
+		Help: "Time of /create PUT",
+	})
+
+	getRequestsNumber = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "Get_requests",
+		Help: "Number of /:tinyurl GET requests",
+	})
+
+	getRequestTime = promauto.NewSummary(prometheus.SummaryOpts{
+		Name: "Get_request_time",
+		Help: "Time of GET",
+	})
+)
 
 func setupRouter(db *sql.DB) *gin.Engine {
 	r := gin.Default()
@@ -58,32 +84,45 @@ func setupRouter(db *sql.DB) *gin.Engine {
 	})
 
 	r.PUT("/create", func(c *gin.Context) {
-		body_request := Request{}
-		err := c.BindJSON(&body_request)
+		createRequestsNumber.Inc()
+		start := time.Now()
+
+		bodyRequest := Request{}
+		err := c.BindJSON(&bodyRequest)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"response": "bad long url"})
 			return
 		}
-		_, err = db.Exec("INSERT INTO urls (url) VALUES ($1) ON CONFLICT DO NOTHING", body_request.Long_url)
+		_, err = db.Exec("INSERT INTO urls (url) VALUES ($1) ON CONFLICT DO NOTHING", bodyRequest.Long_url)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"response": "can't insert new long url: " + err.Error()})
 			return
 		}
 
 		var id int
-		err = db.QueryRow("SELECT id FROM urls WHERE url = $1", body_request.Long_url).Scan(&id)
+		err = db.QueryRow("SELECT id FROM urls WHERE url = $1", bodyRequest.Long_url).Scan(&id)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"response": "can't retrieve just inserted url id: " + err.Error()})
 			return
 		}
 
 		tinyUrl := GetTinyUrlById(id)
-		c.JSON(http.StatusOK, gin.H{"longurl": body_request.Long_url, "tinyurl": tinyUrl})
+		c.JSON(http.StatusOK, gin.H{"longurl": bodyRequest.Long_url, "tinyurl": tinyUrl})
+
+		elapsed := float64(time.Since(start).Milliseconds())
+		createRequestTime.Observe(elapsed)
 	})
 
 	r.GET("/:url", func(c *gin.Context) {
+		getRequestsNumber.Inc()
+		start := time.Now()
+
 		tinyUrl := c.Params.ByName("url")
-		id := GetIdByTinyUrl(tinyUrl)
+		id := GetIdByTinyUrl(tinyUrl, c)
+		if tinyUrl != GetTinyUrlById(id) {
+			c.Writer.WriteHeader(404)
+			return
+		}
 		var longUrl string
 		err := db.QueryRow("SELECT url FROM urls WHERE id = $1", id).Scan(&longUrl)
 		if err != nil {
@@ -91,6 +130,9 @@ func setupRouter(db *sql.DB) *gin.Engine {
 		} else {
 			c.Redirect(http.StatusFound, longUrl)
 		}
+
+		elapsed := float64(time.Since(start).Milliseconds())
+		getRequestTime.Observe(elapsed)
 	})
 
 	return r
@@ -120,6 +162,13 @@ func StartSQL() *sql.DB {
 
 func main() {
 	conn := StartSQL()
+
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		err := http.ListenAndServe(":2112", nil)
+		ErrorCheck(err, "ERROR: Cannot connect to Prometheus")
+	}()
+
 	r := setupRouter(conn)
 	r.Run(":8080")
 }
